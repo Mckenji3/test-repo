16e3a3c7f9c883b74f2c42efdc56a1ca43a26987
Mckenji3 <140677625+Mckenji3@users.noreply.github.com>
2025-02-14 14:07:56 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,18 +1,14 @@
-<?php 
+<?php
+header('Content-Type: application/json');
 
-    header('Content-Type: application/json');
-
-    if ($_SERVER['REQUEST_METHOD'] === 'GET' ) {
-
-        $response = array(
-            'message' => 'Hello, World!' 
-        );
-    
-    }else {
-        
-        http_response_code(405); 
-        echo json_encode(array('error' => 'Method Not Allowed'));
-    }
-
-
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $response = array(
+        'message' => 'Hello, World!'
+    );
+    echo json_encode($response);
+} else {
+    http_response_code(405); // Method Not Allowed
+    echo json_encode(array('error' => 'Method Not Allowed'));
+    exit; // Stop script execution after sending the error response
+}
 ?>
