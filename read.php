12d699b121a177785bc548f3bbe9580757a7f86e
Mckenji3 <140677625+Mckenji3@users.noreply.github.com>
2025-02-14 17:47:54 +0800

    Update read.php

--- read.php
+++ read.php
@@ -1,5 +1,6 @@
 <?php
-    require('/database.php');
+    // Include database.php from the root folder
+    require(__DIR__ . '/../database.php');
 
     $queryAccounts = "SELECT * FROM accounts";
     $sqlAccounts = mysqli_query($connection, $queryAccounts);
